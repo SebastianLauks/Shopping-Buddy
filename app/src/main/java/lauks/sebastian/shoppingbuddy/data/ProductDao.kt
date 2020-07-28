c6525e09a076e7689ecda0e8c57cc9c6ecaaaf7f
Sebastian <sebastian.lauks13@gmail.com>
2020-07-28 22:23:55 +0200

    connected to firebase and added possibility of adding products to database and they automatically display in recycler view

--- app/src/main/java/lauks/sebastian/shoppingbuddy/data/ProductDao.kt
+++ app/src/main/java/lauks/sebastian/shoppingbuddy/data/ProductDao.kt
@@ -1,21 +1,51 @@
 package lauks.sebastian.shoppingbuddy.data
 
+
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ProductDao {
+    private lateinit var database: DatabaseReference
     private val productList = mutableListOf<Product>()
     private val products = MutableLiveData<List<Product>>()
 
     init {
         products.value = productList
+        database = Firebase.database.reference
+
+        val productsListener = object: ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                productList.clear()
+                snapshot.children.forEach{product: DataSnapshot ->
+                    val productMap = product.value as HashMap<String,*>
+                    productList.add(Product(product.key as String, productMap.get("name") as String))
+                }
+                products.value = productList
+            }
+
+        }
+
+        database.child("Products").addValueEventListener(productsListener)
     }
 
     fun addProduct(product: Product){
-        productList.add(product)
-        products.value = productList
+//        productList.add(product)
+//        products.value = productList
+        database.child("Products").child(product.id).setValue(product)
     }
+
+
 
     fun getProducts() = products as LiveData<List<Product>>
 }
